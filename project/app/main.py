2bd79e30805bcd3a3b61152116322bd923174ca8
marbros <enigmail333@yahoo.es>
2022-04-10 23:08:54 -0500

    small changes are added

--- project/app/main.py
+++ project/app/main.py
@@ -12,7 +12,7 @@ log = logging.getLogger("uvicorn")
 
 
 def create_application() -> FastAPI:
-    application = FastAPI(title="Bla API", description="Bla description")
+    application = FastAPI(title="Summaries API", description="Bla description")
     application.include_router(ping.router)
     application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
 
